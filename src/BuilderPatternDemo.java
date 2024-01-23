e323a69156f58425612828b9c917b48e742392ec
AndreyK1993 <andrey.kurilenko27@gmail.com>
2024-01-23 17:54:12 +0200

    Added file read me

--- src/BuilderPatternDemo.java
+++ src/BuilderPatternDemo.java
@@ -5,12 +5,12 @@ public class BuilderPatternDemo {
         OrderBuilder orderBuilder = new OrderBuilder();
 
         Booking vegBooking = orderBuilder.prepareOrder1();
-        System.out.println("Veg Meal");
+        System.out.println("Order 1");
         vegBooking.showItems();
         System.out.println("Total Cost: " + vegBooking.getCost());
 
         Booking nonVegBooking = orderBuilder.prepareOrder2();
-        System.out.println("\nNon-Veg Meal");
+        System.out.println("\nOrder 2");
         nonVegBooking.showItems();
         System.out.println("Total Cost: " + nonVegBooking.getCost());
     }
